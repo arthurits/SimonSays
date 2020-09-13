2fefcaefd52393cd92098bb61d48e3344ed0c92d
arthurits <simply_alfy@hotmail.com>
2020-09-13 18:50:25 +0200

    Code updated

--- Simon says/Properties/AssemblyInfo.cs
+++ Simon says/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Simon says")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A recreation of the classic Simon game invented by Ralph H. Baer and Howard J. Morrison")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Arthurits Ltd.")]
 [assembly: AssemblyProduct("Simon says")]
 [assembly: AssemblyCopyright("Copyright ©  2014 — 2020")]
 [assembly: AssemblyTrademark("")]
