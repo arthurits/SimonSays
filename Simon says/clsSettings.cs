41688a4aaae6c7e0fc97903fb3791e2b5a718708
arthurits <63915486+arthurits@users.noreply.github.com>
2022-04-04 11:55:33 +0200

    Update default value for WindowPosition property

--- Simon says/clsSettings.cs
+++ Simon says/clsSettings.cs
@@ -14,7 +14,7 @@ public class ClassSettings
     /// Remember window position on start up
     /// </summary>
     [JsonPropertyName("Window position")]
-    public bool WindowPosition { get; set; } = false;
+    public bool WindowPosition { get; set; } = true;
 
     /// <summary>
     /// Window top-left x coordinate
